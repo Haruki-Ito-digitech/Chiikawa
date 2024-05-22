f0481c7be2bde122fa62d021f2abe6b7a5cb9ec4
Haruki-Ito-digitech <ito.haruki.rc@mynavi.jp>
2024-05-22 04:20:45 +0000

    【解答】素数を判定しよう

---
+++ ito/prime.py
@@ -0,0 +1,36 @@
+"""素数を判定しよう"""
+#import
+import math
+import sys
+args = sys.argv
+
+#引数を変数に代入
+num = int(args[1])
+
+#素数判定処理
+def Prime_judge(num):
+    #2~numまで計算を繰り返す
+    for i in range(2, num):
+        #割り切れたら素数ではない
+        if num % int(i) == 0:
+            judge = "not"
+            #素数ではないと分かった時点で処理終了
+            break
+        else:
+        #割り切れなければ素数の可能性あり
+            judge = "Prime"
+    return judge
+
+#メイン関数
+def main():
+    #1000未満かどうか判定
+    if 1000 <= num:
+        judge = "1000以上は判定できません"
+    else:
+        #素数判定
+        judge = Prime_judge(num)
+    
+    print(judge, end="")
+
+if __name__ == '__main__':
+    main()
