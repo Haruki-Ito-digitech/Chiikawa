9ed259b3dec34b36b12e07f1050a7b78a1d0efac
WarabiMorota <morota.warabi.jj@mynavi.jp>
2024-05-22 08:32:27 +0000

    add files

---
+++ morota/transinsert.py
@@ -0,0 +1,33 @@
+import sys
+import datetime
+args = sys.argv
+
+from datetime import date 
+from database import session
+from transinsert_table import Transport
+from sqlalchemy.exc import IntegrityError
+
+#日付読み込み
+day = datetime.datetime.strptime(sys.argv[1], "%Y%m%d")
+
+# 登録するデータの編集
+transport = Transport(
+    data = day.date(),
+    seq = int(args[2]),
+    departure = args[3],
+    arrival = args[4],
+    via = args[5],
+    amount = int(args[6])
+)
+
+# エラー処理
+try:
+    # INSET処理
+    session.add(transport)
+    # コミット
+    session.commit()
+
+except IntegrityError as sqlalchemy_error:
+    print("error:交通費精算テーブルにデータを登録できませんでした")
+except:
+    print("error:交通費精算テーブルにデータを登録できませんでした")
